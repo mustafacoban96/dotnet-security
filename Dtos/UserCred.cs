ec563fb8477c352a1e95c9ac38a97fe72cb81504
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-24 09:39:36 +0300

    user cred dto

---
+++ Dtos/UserCred.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auth_jwt_refresh_mechanism.Dtos
+{
+    public class UserCred
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
