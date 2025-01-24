3997703a5199ebc4f721362f2d0b11baf32228fa
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2025-01-24 09:39:22 +0300

    basic AuthHandler

---
+++ Helpers/BasicAuthenticationHandler.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using auth_jwt_refresh_mechanism.Repository;
+using Azure.Core;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
+
+namespace auth_jwt_refresh_mechanism.Helpers
+{
+    public class BasicAuthenticationHandler: AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder,ApplicationDbContext context) : base(options, logger, encoder)
+        {
+            _context = context;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+             if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("No header found");
+            }
+            var headervalue = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+            
+            if(headervalue != null){
+                var bytes = Convert.FromBase64String(headervalue.Parameter);
+                string credentials=Encoding.UTF8.GetString(bytes);
+                string[] array = credentials.Split(":");
+                string username = array[0];
+                string password = array[1];
+                var user =await _context.TblUsers.FirstOrDefaultAsync(item => item.Username == username && item.Password == password);
+
+                if (user != null)
+                {
+                    var claim = new[] { new Claim(ClaimTypes.Name, user.Username) };
+                    var identity = new ClaimsIdentity(claim, Scheme.Name);
+                    var principal = new ClaimsPrincipal(identity);
+                    var ticket = new AuthenticationTicket(principal, Scheme.Name);
+                    return AuthenticateResult.Success(ticket);
+                }
+                else
+                {
+                    return AuthenticateResult.Fail("UnAutorized");
+                }
+            }
+            else
+            {
+                return AuthenticateResult.Fail("Empty header");
+            }
+            
+        }
+    }
+}
